c065a288a11de80fccfe30f0b5a182d0c4eab38d
meke16 <habtamucherinet40@gmail.com>
2025-12-18 23:52:21 +0300

    install and utilize laravel scout for searching in Item model

--- backend/app/Models/Item.php
+++ backend/app/Models/Item.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Item extends Model
 {
     use HasFactory;
+    use Searchable;
 
     protected $fillable = [
         'item_name',
@@ -31,7 +33,30 @@ class Item extends Model
         'condition',
         'image',
     ];
-
+    
+    /**
+     * Get the indexable data array for the model.
+     */
+    public function toSearchableArray()
+    {
+        return [
+            'id' => $this->id,
+            'item_name' => $this->item_name,
+            'part_number' => $this->part_number,
+            'brand' => $this->brand,
+            'unit' => $this->unit ?? '',
+            // Add other fields you want to search
+        ];
+    }
+    
+    /**
+     * Determine which fields should be used for searching.
+     * This helps the database driver know which columns to index.
+     */
+    public function searchableFields()
+    {
+        return ['item_name', 'part_number', 'brand'];
+    }
     // In Item model
     public function toArray()
     {
