e441ebf26769c42305f79818f74b0fd1111a8064
meke16 <habtamucherinet40@gmail.com>
2025-12-20 18:25:36 +0300

    modify relationship

--- backend/app/Models/Department.php
+++ backend/app/Models/Department.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Department extends Model
 {
@@ -14,5 +13,5 @@ class Department extends Model
         'name',
         'description',
         'status',
-        ];
+    ];
 }
